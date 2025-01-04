ee71711f4e5869a92c420e1e436b18301a8ef3e1
filipaBas <filipa.s.bastos@gmail.com>
2025-01-04 14:02:29 +0000

    update contract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,13 @@ import * as ethContract from "@/contracts/eth";
 import * as gameContract from "@/contracts/game";
 import dynamic from "next/dynamic";
 import { GAME_CONTRACT } from "./constants";
-import { useEffect, useState } from "react";
 
 const WalletBar = dynamic(() => import("../components/ui/WalletBar"), {
   ssr: false,
 });
 
 export default function Home() {
-  const { address: userAddress, connector } = useAccount();
-
-  const [approveAmount, setApproveAmount] = useState<string>("0.1");
-  const [betAmount, setBetAmount] = useState<string>("0.0002");
-  const [remainingApproval, setRemainingApproval] = useState<string>("0");
-  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const { address: userAddress } = useAccount();
 
   // contracts
   const { contract: ethContractInstance } = useContract({
@@ -65,15 +59,6 @@ export default function Home() {
     watch: true,
     refetchInterval: 10,
   });
-  
-  const convertEthToWei = (ethAmount: string): string => {
-    try {
-      return BigInt(Math.round(parseFloat(ethAmount) * 10 ** 18)).toString();
-    } catch (error) {
-      console.error("Conversion error:", error);
-      return "0";
-    }
-  };
 
   // Place bet
   const play = async () => {
@@ -83,7 +68,7 @@ export default function Home() {
           gameContractInstance.address,
           0.00002 * 10 ** 18,
         ]),
-         // half a dollar (0.5 USD) would be approximately 0.0001643485 ETH - converting to wei = 1.643485 * 10 ** 14
+        // half a dollar (0.5 USD) would be approximately 0.0001643485 ETH - converting to wei = 1.643485 * 10 ** 14
         // should pass this to test on starkscan 164348500000000000
         gameContractInstance.populate("place_bet", [0.000002 * 10 ** 18]),
       ]);
@@ -99,42 +84,6 @@ export default function Home() {
 
   console.log({ prizePool, points });
 
-  const handleModalClose = () => {
-    setRemainingApproval(convertEthToWei(approveAmount));
-    setModalOpen(false);
-
-    localStorage.setItem("modalShown", "true");
-  };
-
-  const handleApprovalExhausted = () => {
-    setModalOpen(true);
-    localStorage.removeItem("modalShown");
-  };
-
-  useEffect(() => {
-    const modalAlreadyShown = localStorage.getItem("modalShown");
-    if (!modalAlreadyShown) {
-      // If the modal hasn't been shown before, display it
-      setModalOpen(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (BigInt(remainingApproval) <= 0) {
-      handleApprovalExhausted();
-    }
-  }, [remainingApproval]);
-
-  useEffect(() => {
-    if (!userAddress) {
-      console.log("Wallet disconnected. Resetting state...");
-      // Reset app-specific wallet-related states
-      setRemainingApproval("0");
-      setApproveAmount("0.1");
-      setBetAmount("0.0002");
-    }
-  }, [userAddress]);
-
   const convertWeiToEth = (wei) => (wei ? Number(wei) / 10 ** 18 : null);
   const convertedPrizePool = convertWeiToEth(prizePool);
 
@@ -145,41 +94,6 @@ export default function Home() {
       </div>
       {userAddress ? (
         <>
-          {/*    {isModalOpen && (
-            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-              <div className="bg-white p-6 rounded shadow-lg space-y-4 w-96">
-                <h2 className="text-lg font-bold">Set Your Values</h2>
-                <div className="flex flex-col">
-                  <label className="mb-2">Approve Amount (ETH)</label>
-                  <input
-                    type="number"
-                    step="0.01"
-                    min="0"
-                    value={approveAmount}
-                    onChange={(e) => setApproveAmount(e.target.value)}
-                    className="border p-2 rounded w-full"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label className="mb-2">Bet Amount (ETH)</label>
-                  <input
-                    type="number"
-                    step="0.0001"
-                    min="0"
-                    value={betAmount}
-                    onChange={(e) => setBetAmount(e.target.value)}
-                    className="border p-2 rounded w-full"
-                  />
-                </div>
-                <button
-                  onClick={handleModalClose}
-                  className="border-2 border-blue-500 bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 active:bg-blue-700 transition-all"
-                >
-                  Confirm
-                </button>
-              </div>
-            </div>
-          )} */}
           <div className="flex flex-col items-center space-y-4 w-full max-w-md">
             <button
               onClick={play}
