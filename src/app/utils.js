0100d2c0cb6bb1da8960255d24ba4f5947bcdff1
Diana Bastos <bastosdsb@gmail.com>
2025-02-01 13:47:47 +0000

    (fix) Starknet.js execute function migration to V3

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,13 +1,19 @@
-import { RpcProvider, Account } from "starknet";
+import { RpcProvider, Account, constants } from "starknet";
 import "dotenv/config";
 
 const provider = new RpcProvider({
   nodeUrl: process.env.NODE_URL,
   headers: {
     "x-apikey": process.env.API_KEY,
-  }
+  },
 });
 
-const account = new Account(provider, process.env.ACCOUNT_ADDRESS_SEPOLIA, process.env.ACCOUNT_PRIVATE_KEY_SEPOLIA);
+const account = new Account(
+  provider,
+  process.env.ACCOUNT_ADDRESS_SEPOLIA,
+  process.env.ACCOUNT_PRIVATE_KEY_SEPOLIA,
+  undefined,
+  constants.TRANSACTION_VERSION.V3
+);
 
 export { provider, account };
